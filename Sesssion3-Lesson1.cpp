9e9204e03379c4b34efdbb0f4e67b484097db264
Hoang2006-creater <duhoang1611@gmail.com>
2024-11-21 19:55:30 +0700

    Add files via upload

---
+++ Sesssion3-Lesson1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	// Khai bao bien
+	char ten[100];
+	// yeu cau nhap ten
+	printf("Moi ban nhap ten cua ban: ");
+	scanf("%s", ten);
+	// Hien thi loi chao
+    printf("Xin chao %s\n", ten);
+    
+    return 0;	
+}
